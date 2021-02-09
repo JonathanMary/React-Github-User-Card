3ae06c25b460439d5a9fd92b2c17e9a81aa24f9f
JonathanMary <jon85ma@gmail.com>
2021-02-09 22:49:09 +0100

    MVP: Done?

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      userinfo: {},
+    }
+  };
+
+  componentDidMount(){
+    axios.get(`https://api.github.com/users/JonathanMary`)
+         .then(res => {
+           this.setState({userinfo: res.data});
+         })
+         .catch(err => console.log(err))
+  }
+
+
+  render(){
+    console.log(this.state.userinfo)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>{this.state.userinfo.name}</h1>
+          <img src={this.state.userinfo.avatar_url} alt="avatar"></img>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
